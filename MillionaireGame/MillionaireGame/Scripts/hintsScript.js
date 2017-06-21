b3c53c45fdeef89a046b566a2393acbb74b42613
Ulyana Zhovtanetska <ulyana.zhovtanetska@gmail.com>
2017-06-21 21:43:18 +0300

    Implemented modal view with "Ask Audience" hint.

--- MillionaireGame/MillionaireGame/Scripts/hintsScript.js
+++ MillionaireGame/MillionaireGame/Scripts/hintsScript.js
@@ -5,6 +5,10 @@ $("#hint50crossed").hide();
 $("#friendCall").on("click", friendCallHint);
 $("#friendCallCrossed").hide();
 
+$("#audienceAsk").on("click", AskAudienceHint);
+$("#audienceAskCrossed").hide();
+
+
 function fiftyHint() {
     $.ajax({
         url: window.redirectToFiftyUrl,
@@ -27,6 +31,21 @@ function friendCallHint() {
     
 }
 
+function AskAudienceHint() {
+   $.ajax({
+        type: "GET",
+        url: window.redirectToAskAudience,
+        contentType: "application/html; charset=utf-8",
+        data: { questionId: window.questionId },
+        datatype: "html",
+        success: function (result) {  
+            $('#audienceModalContent').html(result);
+            $('#audienceModal').modal('show');
+            $("#audienceAsk").hide();
+            $("#audienceAskCrossed").show();
+        }
+    });
+}
 
 function changeHint50Image() {
     $("#hint50").hide();
